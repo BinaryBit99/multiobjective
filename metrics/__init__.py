2a0310e275b9a99fc1a071f85c10e227476c16c1
Evan Barker <111829951+BinaryBit99@users.noreply.github.com>
2025-08-19 16:35:45 -0600

    chore: tidy SCS config implementation

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,3 +1,9 @@
 from .scs import SCSConfig, SCSComponents, scs, expected_scs_next
 
-__all__ = ["SCSConfig", "SCSComponents", "scs", "expected_scs_next"]
+# Re-export the primary SCS utilities for convenient external use.
+__all__ = (
+    "SCSConfig",
+    "SCSComponents",
+    "scs",
+    "expected_scs_next",
+)
